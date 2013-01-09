8cfbda5694391ddbdbe2fd2d7080e1f99bfeab2a
jungbunzlav <christian.petersen@gmx.de>
2013-01-09 15:15:44 +0100

    migration to spree 1.3

--- app/overrides/invoice.rb
+++ app/overrides/invoice.rb
@@ -3,15 +3,3 @@ Deface::Override.new(:virtual_path => "spree/admin/orders/index",
                      :insert_top => "[data-hook='admin_orders_index_row_actions'], #admin_orders_index_row_actions[data-hook]",
                      :partial => "spree/admin/orders/invoice_link",
                      :disabled => false)
-                     
-Deface::Override.new(:virtual_path => "spree/admin/orders/edit",
-                     :name => "invoice_botton_edit",
-                     :insert_top => "[data-hook='admin_order_edit_buttons'], #admin_order_edit_buttons[data-hook]",
-                     :partial => "spree/admin/orders/invoice_button",
-                     :disabled => false)
-                     
-Deface::Override.new(:virtual_path => "spree/admin/orders/show",
-                     :name => "invoice_botton_show",
-                     :insert_top => "[data-hook='admin_order_show_buttons'], #admin_order_show_buttons[data-hook]",
-                     :partial => "spree/admin/orders/invoice_button",
-                     :disabled => false)
